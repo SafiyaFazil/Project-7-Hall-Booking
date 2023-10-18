d87653c8e5c89ecc3c38a64010c7efd2fda4b3e6
Safiya@0619 <saficute05@gmail.com>
2023-10-18 12:43:35 +0530

    Welcome comment add

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use("/hallBooking", HallBookingRouter);
 
+app.get('/', (req, res) => {
+    res.status(200).send("Welcome")
+})
+
 app.listen(PORT, () => {
     console.log("My App is Listening Port: ",PORT);
 })
